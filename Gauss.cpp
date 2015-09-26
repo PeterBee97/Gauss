c0c12a3d87c403bb2e876a77d0fb44cb34d0cbc8
PeterBee97 <bjd97@qq.com>
2015-09-27 01:04:42 +0800

    approximate frame

--- Gauss.cpp
+++ Gauss.cpp
@@ -1,14 +1,47 @@
 #include<iostream>
+void swap(int x,y)
+{
+	int k,temp;
+	for (k = 0; k <= n; ++k)
+			{
+				temp:=a[x][k];
+				a[x][k]=a[y][k];
+				a[y][k]=temp;
+			}
+}
+
+void del(x,l)
+{
+
+}
+
 int main()
 {
-	int a[100][100],i,j,k,n;
-	for (int i = 0; i < n; ++i)
+	int a[100][101],temp,i,i2,j,k,n;
+	for (i = 0; i < n; ++i)
 	{
-		for (int j = 0; j < n; ++j)
+		for (j = 0; j <= n; ++j)
 		{
 			cin>>a[i][j];
 		}
+	}	//init
+	for (i = 0; i < n; ++i)
+	{
+		if (a[i][i]=0)
+		{
+			for (i2 = i+1; i2 < n; ++i2)
+			{
+				if(a[i2][i]!=0)
+				{
+					swap(i,i2);
+					i2=n+1;
+				}
+			}
+		}else
+		for (i2 = i+1; i2 < n; ++i2)
+		{
+			del(a[i][i],i2);
+		}
 	}
-
 	return 0;
 }
