013d14514b24957e6a8e33ce343b7b6b55173320
PeterBee97 <bjd97@qq.com>
2015-09-28 10:33:28 +0800

    First Compile Success

--- Gauss.cpp
+++ Gauss.cpp
@@ -1,23 +1,45 @@
 #include<iostream>
-void swap(int x,y)
+using namespace std;
+int a[100][101],temp,i,i2,j,k,n;
+void swap(int x,int y)
 {
 	int k,temp;
 	for (k = 0; k <= n; ++k)
 			{
-				temp:=a[x][k];
+				temp=a[x][k];
 				a[x][k]=a[y][k];
 				a[y][k]=temp;
 			}
 }
 
-void del(x,l)
+int gcd(int m,int n)
 {
+	return m%n?gcd(n,m%n):n;
+}
 
+void del(int l1,int l2)
+{
+	int g=gcd(a[l1][l1],a[l2][l1]);
+	for (int i = l1; i <= n; ++i)
+	{
+		a[l1][i]*=(a[l2][l1]/g);
+		a[l2][i]*=(a[l1][l1]/g);//get lcm
+		a[l2][i]-=a[l1][i];//delete
+	}
+
+}
+
+void print()
+{
+	for (int i = 0; i <n; ++i)
+	{
+		for(int j=0;j<=n;j++)cout<<a[i][j]<<" ";
+			cout<<endl;
+	}
 }
 
 int main()
 {
-	int a[100][101],temp,i,i2,j,k,n;
 	for (i = 0; i < n; ++i)
 	{
 		for (j = 0; j <= n; ++j)
@@ -40,8 +62,9 @@ int main()
 		}else
 		for (i2 = i+1; i2 < n; ++i2)
 		{
-			del(a[i][i],i2);
+			del(i,i2);//将第i行化为0
 		}
+		print;
 	}
 	return 0;
 }
